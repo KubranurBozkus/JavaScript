59e04d3615e13a8632ed0f516a9df60308344f5a
Kübranur Bozkuş <110054318+KubranurBozkus@users.noreply.github.com>
2024-03-21 17:23:09 +0300

    soru cevap

--- cevaplar.js
+++ cevaplar.js
@@ -6,3 +6,20 @@ let mod = "kötü"
  //tekrar güncelle
  mod = "daha iyi"
  console.log("modum şu an:",mod)
+
+ //cevap 2
+ let adim = "ömer"
+ let soyadim = "ocak"
+ let yas = 24
+ console.log("adım:", adim, "soyadım:", soyadim, "yaşım:", yas)
+
+ //cevap 3
+
+ const ben = {
+    adim : "kübranur",
+    yasim : 26,
+    yasadigimYer:"istanbul",
+    
+ }
+
+ console.log("adım:", ben.adim , "yaşım:", ben.yasim)
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // let mod = "kötü" bu değişkeni iki kez güncelleyin ve her güncellemeyi konsola yazdırın örnek güncelleme 
 //(iyi, daha kötü vb.)
 
+//3 adet tipleri 2 tane string ve 1 tane number olacak şeklde değişken oluşturun ve bu değişkenleri 
+//konsola yazdrın
+
+//Adınızı,yaşınızı,yaşadığınız yeri ve doğum gününüzü obje tipinde sabit değişken olarak oluşturun,
+//konsola adınızı ve yaşınızı yazdırın
