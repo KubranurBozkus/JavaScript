9b53e4731db8f009b2431da1ec8827f70c8a1571
Kübranur Bozkuş <110054318+KubranurBozkus@users.noreply.github.com>
2024-03-21 16:49:21 +0300

    yeni sayfalar eledim

---
+++ cevaplar.js
@@ -0,0 +1,8 @@
+let mod = "kötü"
+ //güncelle
+ mod = "iyi"
+ console.log("modum şu an:",mod)
+
+ //tekrar güncelle
+ mod = "daha iyi"
+ console.log("modum şu an:",mod)
---
+++ script.js
@@ -0,0 +1,3 @@
+// let mod = "kötü" bu değişkeni iki kez güncelleyin ve her güncellemeyi konsola yazdırın örnek güncelleme 
+//(iyi, daha kötü vb.)
+
