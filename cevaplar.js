3599501d6b5f1c74c97eec1cc1ad54a673969303
Kübranur Bozkuş <110054318+KubranurBozkus@users.noreply.github.com>
2024-03-24 17:23:30 +0300

    soruların çözümleri

--- cevaplar.js
+++ cevaplar.js
@@ -23,3 +23,99 @@ let mod = "kötü"
  }
 
  console.log("adım:", ben.adim , "yaşım:", ben.yasim)
+
+ //cevap 4
+
+ let bisey = "lorem ipsum"
+
+ console.log("büyük hali:", bisey.toLocaleUpperCase())
+
+ //cevap 5
+
+ let cumle = "zabağan zor uyanıoz napacağız biz"
+
+ //hatalı kelimeleri düzelt
+
+ cumle = cumle.replace("zabağan","sabah")
+ cumle = cumle.replace("uyanıoz","uyanıyoruz")
+ cumle = cumle.replace("napacağız","ne yapacağız")
+
+ console.log("güncellenmiş hali:",cumle)
+ 
+ //cevap 6
+
+ const array = ["muz","kiraz","armut","haha", "elma"]
+ //hahayı çıkar.
+
+ array.splice(3,1)
+ console.log("meyveler:",array)
+
+ //yer değiştir.
+ array[0] = "elma"
+ array[3] = "muz"
+
+ console.log("yeni dizi:",array)
+
+ //cevap 7
+
+ array.unshift("portakal")
+ console.log("portakal eklenmiş hali:",array)
+
+ //cevap 8
+const meyvelerimiz = ["elma","kiraz","armut","erik"]
+
+ const kisi = {
+   ad:"Kübra",
+   yas:"26",
+   favoriMeyve:["karpuz"]
+ }
+
+ const iceriyomu = meyvelerimiz.includes(kisi.favoriMeyve[0])
+ console.log("içeriyomu?:",iceriyomu)
+
+ //cevap 9
+
+ const ahmetinHatasi = ["başarılı","bir","şekilde","işeminiz","gerçekleşmiştir","teşekkür","ederiz","."]
+ //string olarak gönder.
+ const stringOlarak = ahmetinHatasi.join(" ")
+ console.log("Ahmetin inputu:",stringOlarak)
+
+ //cevap 10
+
+ const cumle2 ="ldkflşdf lorem ipsum dolor dnfkndfkdfn."
+
+ //cümle nktayla bitiyor mu?
+
+ const bitiyormu = cumle2.endsWith(".")
+ console.log("cümlee:",bitiyormu)
+
+ //cevap 11
+
+ const kisi1 = {ad: "burak", yas:1 , eskiIsimler:[] }
+
+ //prompt
+ const yeniIsim = "ömer"
+ 
+ //yeni ismi değitir
+ kisi1.eskiIsimler.push(kisi1.ad)
+ kisi1.ad = yeniIsim
+
+ console.log("YENİ HALİ:", kisi1)
+
+ //cevap 12
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -6,3 +6,32 @@
 
 //Adınızı,yaşınızı,yaşadığınız yeri ve doğum gününüzü obje tipinde sabit değişken olarak oluşturun,
 //konsola adınızı ve yaşınızı yazdırın
+
+// Bir string oluşturun konsola ise onu büyük harflerle yazdırın.
+
+//zabağan zor uyanıoz napacağız biz. Bu stringde kelimeleri türkçe karakterlere uygun bir şekilde değiltirin
+//ve cümlenin sonuna nokta yerine ? koyun.
+
+//muz,kiraz,armut haha elma bu dizie elma olmayan keimeyi çıkartın.
+//Yukarıdaki dizinin ik elemanını elma, 3. elemanını muz yapın.
+
+//dizinin başına portakal ekleyin.
+
+//Bir obje oluşturun ve objeye bir insan modellemesi (ad, yaş,favori meyve vs.) yapın oluşturmuş olduğunuz 
+//bu objede kişinin favori meyvesinin ["elma","kiraz","armut","erik"] bu arrayda olup olmadığını konrol edin.
+
+//Ahmet adlı yazılımcı ilgili taskları yazdıktan sonra client tarafına şu mesajı göndermeye çalışıyor;
+//["başarılı","bir","şekilde","işeminiz","gerçekleşmiştir","teşekkür","ederiz","."] 
+//ancak mesaj bir dizi(array) olarak gitmekte, ahmete sorunu çözebilmesi için yardım edin.
+
+//Forum yöneticisi tüm cümlelerin sonunda nokta (.) olmasını istiyor, cümlelerin sonunda nokta 
+//olup olmadığını kontrol edin.
+
+
+//{ad: "burak", yas:1 , eskiIsimler:[] } bu objede adı değiştirin ve eskiIsimler dizisine önceki ismi kayıt edin.
+
+
+
+
+
+
